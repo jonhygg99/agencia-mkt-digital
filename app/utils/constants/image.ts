4469029e5d9b582358565d081152492caddd33db
Jonathan Gómez <jonhygg99@gmail.com>
2025-04-21 18:34:35 +0200

    fixed image not found

--- app/utils/constants/image.ts
+++ app/utils/constants/image.ts
@@ -7,7 +7,7 @@ export const IMAGE_SLUG_AGENCIA = `/agencia.webp`;
 export const IMAGE_SCHEMA_AUDITORIA_SEO = `${DOMINIO}/service/seo/Servicio-Auditoria-SEO.webp`;
 export const IMAGE_SCHEMA_CONSULTORIA_SEO = `${DOMINIO}/service/seo/Servicio-Consultoria-SEO.webp`;
 export const IMAGE_SCHEMA_MANTENIMIENTO_WEB = `${DOMINIO}/service/seo/Servicio-Mantenimiento-Web.webp`;
-export const IMAGE_SCHEMA_POSICIONAMIENTO_SEO = `${DOMINIO}/service/seo/Servicio-Posicionamiento-Web-Profesional-SEO.webp`;
+export const IMAGE_SCHEMA_POSICIONAMIENTO_SEO = `${DOMINIO}/service/seo/Servicio-Posicionamiento-Web-Profesional-con-SEO.webp`;
 export const IMAGE_SCHEMA_SEO_LOCAL = `${DOMINIO}/service/seo/Servicio-Posicionamiento-SEO-Local.webp`;
 
 export const IMAGE_SCHEMA_DISENO_WEB = `${DOMINIO}/service/seo/Servicio-Diseño-Web-a-medida.webp`;
